dbf67a13c2e9027f144da82b5a58d46039d47c0f
MalenaLucero <luceromalena@hotmail.com>
2019-08-18 19:49:55 -0300

    fetch search results

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ const allPopularMovies = () =>{
     hideElement('topRatesSection')
     hideElement('upcomingSection')
     hideElement('nowPlayingSection')
+    hideElement('searchSection')
     fetchMoviePosters('popularMovies', 'popular', twentyArray)
     hideElement('popularViewAll')
     showElement('popularResults')
@@ -55,6 +56,7 @@ const allTopRates = () =>{
     showElement('topRatesSection')
     hideElement('upcomingSection')
     hideElement('nowPlayingSection')
+    hideElement('searchSection')
     fetchMoviePosters('topRates', 'top_rated', twentyArray)
     hideElement('topRatesViewAll')
     showElement('topRatesResults')
@@ -69,6 +71,7 @@ const allUpcoming = () =>{
     hideElement('topRatesSection')
     showElement('upcomingSection')
     hideElement('nowPlayingSection')
+    hideElement('searchSection')
     fetchMoviePosters('upcoming', 'upcoming', twentyArray)
     hideElement('upcomingViewAll')
     showElement('upcomingResults')
@@ -83,11 +86,48 @@ const allNowPlaying = () =>{
     hideElement('topRatesSection')
     hideElement('upcomingSection')
     showElement('nowPlayingSection')
+    hideElement('searchSection')
     fetchMoviePosters('nowPlaying', 'now_playing', twentyArray)
     hideElement('nowPlayingViewAll')
     showElement('nowPlayingResults')
     showElement('nowPlayingLoad')
     clicksCounter('nowPlayingLoad', 'nowPlaying', 'now_playing')
+}
+
+const searchMovie = () =>{
+    hideElement('popularSection')
+    hideElement('topRatesSection')
+    hideElement('upcomingSection')
+    hideElement('nowPlayingSection')
+    showElement('searchSection')
+    innerHTMLCleaner('search')
+    showElement('searchLoad')
+    let input = document.getElementById('searchInput')
+    let searchInput = input.value
+    input.value = ''
+    if(searchInput !== ''){
+        let container = document.getElementById('search')
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchInput}&page=${1}`)
+            .then(res=>res.json())
+            .then(res=>{
+                twentyArray.forEach(num=>{
+                    let li = document.createElement('li')
+                    let anchor = document.createElement('a')
+                    let figure = document.createElement('figure')
+                    let image = document.createElement('img')
+                    let movieTitle = document.createElement('figcaption')
+                    image.src = `https://image.tmdb.org/t/p/w300${res.results[num].poster_path}`
+                    movieTitle.innerText = res.results[num].original_title
+                    figure.appendChild(image)
+                    figure.appendChild(movieTitle)
+                    anchor.appendChild(figure)
+                    li.appendChild(anchor)
+                    container.appendChild(li)
+                })
+                
+            })
+            .catch(error=>console.log(error))  
+    }
 }
 
 const clicksCounter = (buttonId, containerId, category) =>{
